7afaa0d563d2af5df29aaee9f6e38e4bae028d0d
Yeji Kim <66160055+HiBixby@users.noreply.github.com>
2023-07-21 10:05:25 +0900

    ✨ 농작물 수확하기

---
+++ 01주차/농작물수확하기/Kyj.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Kyj {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int tc = 1; tc<T+1;tc++){
+            int n = sc.nextInt();
+            int[][] farm = new int[n][n];
+            for(int i = 0; i<n;i++){
+                String[] tmp = sc.next().split("");
+                for(int j = 0; j<n;j++){
+                    farm[i][j] = Integer.parseInt(tmp[j]);
+                }
+
+            }
+
+            int sum = 0;
+            for(int i = 0; i<(n/2)+1;i++){
+                int len = 2*i+1;
+                int s = (n/2)-(len/2);
+                int e = (n/2)+(len/2);
+                for(int j = s; j<=e;j++){
+                    sum+=farm[i][j];
+                }
+            }
+            for(int i = (n/2)+1;i<n;i++){
+                int len = 2*(n-i)-1;
+                int s = (n/2)-(len/2);
+                int e = (n/2)+(len/2);
+                for(int j = s; j<=e;j++){
+                    sum+=farm[i][j];
+                }
+            }
+            System.out.printf("#%d %d\n",tc,sum);
+        }
+    }
+}
