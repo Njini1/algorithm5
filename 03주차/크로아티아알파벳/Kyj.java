700c9b66af2bbceeade8168c39cc8f356ccb8f99
Yeji Kim <66160055+HiBixby@users.noreply.github.com>
2023-08-01 23:36:04 +0900

    ⚡️ 크로아티아 알파벳

--- 03주차/크로아티아알파벳/Kyj.java
+++ 03주차/크로아티아알파벳/Kyj.java
@@ -2,6 +2,6 @@ import java.util.*;
 class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println(sc.next().replaceAll("dz=","*").replaceAll("(c=|c-|d-|lj|nj|s=|z=)","*").length());
+		System.out.println(sc.next().replaceAll("dz=|c=|c-|d-|lj|nj|s=|z=","*").length());
 	}
 }
