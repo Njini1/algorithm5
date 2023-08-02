06d4150e1e28ef9361cdc37ad50b2703f8cf6953
Yeji Kim <66160055+HiBixby@users.noreply.github.com>
2023-08-02 17:49:23 +0900

    Create Kyj.java

---
+++ 03주차/원재의 메모리 복구하기/Kyj.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			char[] memory = sc.next().toCharArray();
+			char prev = '0';
+			int answer = 0;
+			for (int i = 0; i < memory.length; i++) {
+				if (memory[i] != prev) {
+					answer++;
+				}
+				prev = memory[i];
+			}
+			System.out.printf("#%d %d\n",tc,answer);
+		}
+	}
+}
